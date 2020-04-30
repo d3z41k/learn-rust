be1f9417e48e8905809c30860224c7938b0fa4e1
Denis Zhokhov <d3z41k@gmail.com>
2020-04-30 10:04:52 +0200

    Add threads

---
+++ src/26_threads.rs
@@ -0,0 +1,19 @@
+use std::thread;
+
+fn main() {
+    let mut c = vec![];
+
+    for i in 0..10 {
+        c.push(thread::spawn(move || {
+            println!("thread number {}", i);
+        }));
+    }
+
+//    for j in 0..10 {
+//        println!("main thread");
+//    }
+
+    for j in c {
+        j.join();
+    }
+}
